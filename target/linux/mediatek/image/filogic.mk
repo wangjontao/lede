4d1833226d5485dca229c0808bbf0cc67a13cf85
wangjontao <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-09 17:56:11 +0800

    Build XR30 256MB NAND with 238MiB UBI and DulWiFi NPS defaults

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -504,7 +504,7 @@ define Device/cmcc_xr30-nand
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 116736k
+  IMAGE_SIZE := 243712k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
