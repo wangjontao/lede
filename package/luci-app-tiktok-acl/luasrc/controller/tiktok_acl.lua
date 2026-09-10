14e312329b8445f2ede69fda57139ba40d73d900
wangjontao <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-10 12:59:37 +0800

    Include LuCI ACL package in RAX3000M build

---
+++ package/luci-app-tiktok-acl/luasrc/controller/tiktok_acl.lua
@@ -0,0 +1,12 @@
+module("luci.controller.tiktok_acl", package.seeall)
+function index()
+  entry({"admin","network","tiktok_acl"}, call("page"), _("TikTok WiFi ACL"), 80).dependent=false
+end
+function page()
+  local http=require "luci.http"; local uci=require "luci.model.uci".cursor()
+  if http.formvalue("apply") then
+    for i=1,10 do local n="tk"..i; uci:set("tiktok_acl",n,"policy",http.formvalue("policy"..i) or "global") end
+    uci:commit("tiktok_acl"); http.redirect("/cgi-bin/luci/admin/network/tiktok_acl")
+  end
+  luci.template.render("tiktok_acl",{uci=uci})
+end
---
+++ package/luci-app-tiktok-acl/luasrc/model/cbi/tiktok_acl.lua
@@ -0,0 +1,1 @@
+local m=Map("tiktok_acl",translate("TikTok WiFi ACL")); return m
